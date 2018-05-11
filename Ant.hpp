1a038e2ceb1dfd4a0c2b8d8f34c16a8dc371353c
Christopher Beall <beallch@oregonstate.edu>
2018-05-11 12:01:43 -0500

    fix include
    fix include

--- Ant.hpp
+++ Ant.hpp
@@ -8,7 +8,7 @@ Description: Header file for the Ant class.
 #define ANT_HPP
 
 //NEED TO CHANGE THIS FOR FINAL UPLOAD
-#include "c:\Users\Christopher\Documents\GitHub\CS162\Critter.hpp"
+#include "Critter.hpp"
 
 class Ant : public Critter
 {
